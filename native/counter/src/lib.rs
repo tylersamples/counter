c5a027d434bcf443e8246d17366012ec845a39f4
Tyler Samples <samples.tm@gmail.com>
2021-04-16 23:23:46 -0600

    Create Counter struct, CounterArc, Implement the functions for: new, increment, decrement, and read, in rust, and specify Counter as a resource

--- native/counter/src/lib.rs
+++ native/counter/src/lib.rs
@@ -1,19 +1,54 @@
-fn new() -> () {
-    ()
+use rustler::Env;
+use rustler::ResourceArc;
+use std::mem::drop;
+use std::sync::Mutex;
+
+struct Counter {
+    num: Mutex<i64>,
+}
+
+type CounterArc = ResourceArc<Counter>;
+
+#[rustler::nif]
+fn new() -> CounterArc {
+    ResourceArc::new(Counter { num: Mutex::new(0) })
 }
 
 #[rustler::nif]
-fn increment(counter: ()) -> () {
-    ()
+fn increment(counter: CounterArc) -> CounterArc {
+    let mut counter_lock = counter.num.lock().unwrap();
+    *counter_lock = *counter_lock + 1;
+
+    drop(counter_lock);
+
+    counter
 }
 
 #[rustler::nif]
-fn decrement(counter: ()) -> () {
-    ()
-}
-#[rustler::nif]
-fn read(a: ()) -> i64 {
-    0
+fn decrement(counter: CounterArc) -> CounterArc {
+    let mut counter_lock = counter.num.lock().unwrap();
+    *counter_lock = *counter_lock - 1;
+
+    drop(counter_lock);
+
+    counter
 }
 
-rustler::init!("Elixir.Counter", [new, increment, decrement, read]);
+#[rustler::nif]
+fn read(counter: CounterArc) -> i64 {
+    let counter_lock = counter.num.lock().unwrap();
+    let data = *counter_lock;
+
+    drop(counter_lock);
+
+    data
+}
+
+rustler::init!(
+    "Elixir.Counter",
+    [new, increment, decrement, read],
+    load = |env: Env, _| {
+        rustler::resource!(Counter, env);
+        true
+    }
+);
